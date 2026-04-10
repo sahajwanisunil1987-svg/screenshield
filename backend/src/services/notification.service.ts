5052ac0b8b81a5b1859d2df572b556df0cd4cd4a
Vijay kewlani <vijaykewlani231994@gmail.com>
2026-04-10 14:52:34 +0530

    smpt update

--- backend/src/services/notification.service.ts
+++ backend/src/services/notification.service.ts
@@ -5,7 +5,7 @@ import { env } from "../config/env.js";
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
   port: env.SMTP_PORT,
-  secure: false,
+  secure: true,
   auth: {
     user: env.SMTP_USER,
     pass: env.SMTP_PASS
